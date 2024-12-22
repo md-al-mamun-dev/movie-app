e1fe6e1e48aa833f35c4e05c564af126fb7bf5c0
MD.AL MAMUN <mamunofficialmail@gmail.com>
2024-12-22 19:24:35 +0600

    ....

--- app/user/watchLater/useMovieDetails.js
+++ app/user/watchLater/useMovieDetails.js
@@ -47,7 +47,7 @@ export default function useMovieDetails(id) {
         return () => {
             controller.abort();
         };
-    }, [url]);
+    }, [url, fetchData]);
 
     const refetch = () => {
         const controller = new AbortController();
